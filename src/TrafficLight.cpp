36f83f42575e2a3afaf10245e456c69fb1134289
Simon <you@example.com>
2020-04-20 17:15:55 +0200

    Complete FP.4

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -18,6 +18,9 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lck(_mtx);
+    _queue.emplace_back(std::move(msg));
+    _cond.notify_one();
 }
 
 
